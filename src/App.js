6ddae916ed04366567a1dff0a358891db932b600
Bayley Mauger <bayleymauger@gmail.com>
2018-02-09 21:59:51 +1100

    dry code fix

--- src/App.js
+++ src/App.js
@@ -21,30 +21,24 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // MOVIES //
-    // POPULAR
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => {this.setState({mPopular: response.results})});
-    // NOW PLAYING
-    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({...this.state, mNowPlaying: response.results}));
-    // COMING SOON
-    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({...this.state, mComingSoon: response.results}));
-    // TOP RATED
-    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({...this.state, mTopRated: response.results}));
-
+    let getData = (url, stateObj) => {
+      fetch(url).then(res => {return res.json()}).then(res => {this.setState({[stateObj]: res.results})});
+    }
+    // MOVIES
+    getData('https://api.themoviedb.org/3/movie/popular?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1', 'mPopular');
+    getData('https://api.themoviedb.org/3/movie/now_playing?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1', 'mNowPlaying');
+    getData('https://api.themoviedb.org/3/movie/upcoming?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1','mComingSoon');
+    getData('https://api.themoviedb.org/3/movie/top_rated?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1','mTopRated');
     // TV SHOWS
-    // ON AIR
-    fetch('https://api.themoviedb.org/3/tv/on_the_air?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({tOnAir: response.results}));
-    // AIRING TODAY
-    fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({tAirToday: response.results}));
-    // TOP RATED
-    fetch('https://api.themoviedb.org/3/tv/top_rated?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1').then(res => {return res.json()}).then(response => this.setState({tTopRated: response.results}));
-
+    getData('https://api.themoviedb.org/3/tv/on_the_air?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1', 'tOnAir');
+    getData('https://api.themoviedb.org/3/tv/airing_today?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1', 'tAirToday');
+    getData('https://api.themoviedb.org/3/tv/top_rated?api_key=1208e42d5f9244b06b09af5465f9e155&language=en-US&page=1','tTopRated');
     // NEWS
     fetch('https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=94d4df4aa6f64c6aaf367aa821ccde0c').then(res => res.json()).then(response => this.setState({news: response.articles}));
 
     setTimeout(() => {
       document.getElementById("loader").style.display = 'none';
-    }, 1500);
+    }, 2000);
   }
 
   render() {
@@ -73,7 +67,7 @@ class App extends Component {
         </section>
         <section className="trailer-section">
           <div className="content-wrapper">
-            
+
           </div>
         </section>
       </div>
--- src/Components/News/NewsItem.js
+++ src/Components/News/NewsItem.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import ReactHtmlParser from 'react-html-parser';
 
 export default class NewsItem extends Component {
   render() {
